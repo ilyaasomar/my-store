1192b87fbe05b981bc41583ec9cbeeffc2edc46d
ilyaas <ilyaascumar10@gmail.com>
2022-12-10 12:49:17 +0300

    poused 1:15

---
+++ src/components/List/List.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Card from '../Card/Card'
+const List = () => {
+    const data = [
+        {
+            id: 1,
+            img: 'https://images.pexels.com/photos/5614119/pexels-photo-5614119.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/5614119/pexels-photo-5614119.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            title: 'T-shirt',
+            isNew: true,
+            oldPrice: 19,
+            price: 12
+        },
+        {
+            id: 2,
+            img: 'https://images.pexels.com/photos/4247723/pexels-photo-4247723.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/4968390/pexels-photo-4968390.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            title: 'T-shirt',
+            isNew: true,
+            oldPrice: 19,
+            price: 12
+        },
+        {
+            id: 3,
+            img: 'https://images.pexels.com/photos/5882533/pexels-photo-5882533.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/4004161/pexels-photo-4004161.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            title: 'T-shirt',
+            isNew: false,
+            oldPrice: 19,
+            price: 12
+        },
+        {
+            id: 4,
+            img: 'https://images.pexels.com/photos/4004114/pexels-photo-4004114.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            img2: 'https://images.pexels.com/photos/4049870/pexels-photo-4049870.jpeg?auto=compress&cs=tinysrgb&w=1600',
+            title: 'T-shirt',
+            isNew: false,
+            oldPrice: 19,
+            price: 12
+        }
+    ]
+  return (
+    <div>{data.map((item) => (
+        <Card item={item} key={item.id} />
+    ) )}</div>
+  )
+}
+
+export default List
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import List from '../../components/List/List'
 
 const Products = () => {
+  const id = parseInt(useParams().id);
+console.log(id)
   return (
-    <div>Products</div>
+    <div className='products'>
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Category</h2>
+          <div className="itemInput">
+            <input type='checkbox' id='1' value={1} />
+            <label htmlFor="shoes">Shoes</label>
+          </div>
+          <div className="itemInput">
+            <input type='checkbox' id='1' value={1} />
+            <label htmlFor="shoes">Skart</label>
+          </div>
+          <div className="itemInput">
+            <input type='checkbox' id='1' value={1} />
+            <label htmlFor="shoes">Coats</label>
+          </div>
+        </div>
+        <div className="filterPrice">
+          <h2>Filter by Price</h2>
+          <div className="inputItem">
+            <span>0</span>
+            <input type="range" min={0} max={1000} />
+          </div>
+        </div>
+        <div className="filterPrice">
+          <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id='asc' value='asc' name='price' />
+            <label htmlFor="asc">Price (Lowest First)</label>
+          </div> 
+          <div className="inputItem">
+            <input type="radio" id='desc' value='desc' name='price' />
+            <label htmlFor="desc">Price (Highest First)</label>
+          </div> 
+        </div>
+      </div>
+      <div className="right">
+      <img src="https://images.pexels.com/photos/972804/pexels-photo-972804.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className="catImg" />
+      </div>
+      <List />
+    </div>
   )
 }
 
